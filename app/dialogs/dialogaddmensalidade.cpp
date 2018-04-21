721caef256dbb3fb81c03f4a07c74547d6fbaeb5
Carlos Mir de Souza <cmirdesouza@gmail.com>
2018-04-21 16:39:58 +0000

    Criado codigo base do dialogAddMensalidade

---
+++ app/dialogs/dialogaddmensalidade.cpp
@@ -0,0 +1,10 @@
+#include "dialogs/dialogaddmensalidade.h"
+
+DialogAddMensalidade::DialogAddMensalidade(QWidget *parent) : QDialog(parent)//, ui(new Ui::DialogAddMensalidade)
+{
+    //ui->setupUi(this);
+}
+
+DialogAddMensalidade::~DialogAddMensalidade() {
+    //delete ui;
+}
